e464e1c1a27d8dabe7a3f5e391bef3819e15f295
Aldo Fernando Salazar S <salazar.blackhead@gmail.com>
2016-11-28 18:37:10 -0600

    Add files via upload

---
+++ Parpadeo_de_un_LED/Parpadeo_de_un_LED.ino
@@ -0,0 +1,26 @@
+/*
+  Parpadeo de un LED.
+  
+  Con este programa, el Led encendera durante un segundo, posteriormente
+  se apagara por otro segundo y repetira el proceso.
+  
+  Cambiando el valor en la función "Delay", podemos aumentar o reducir
+  el tiempo.
+ */
+ 
+int led = 13;         // Asignamos una constante del tipo entero
+                      // al Pin #13 del Arduino.
+
+void setup() {                
+  
+  pinMode(led, OUTPUT);     // Indica al Arduino que asigne la constante
+                            // led como salida.
+}
+
+
+void loop() {
+  digitalWrite(led, HIGH);   // Escribe un valor alto (5 voltios) en la salida "led".
+  delay(1000);               // Espera durante un segundo.
+  digitalWrite(led, LOW);    // Escribe un valor bajo (0 voltios) en la salida "led".
+  delay(1000);               // Espera durante un segundo.
+}
